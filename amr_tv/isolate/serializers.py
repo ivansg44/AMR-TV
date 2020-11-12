9bf78b94edb739c4c29c6c65d369ada69f968a66
Ivan Sohrab Gill <isgill93@student.ubc.ca>
2020-11-12 15:12:59 -0800

    Revert "wip"
    This reverts commit b30624bc5a461d92cf59a90177ed197cdb6a8cef.

--- amr_tv/isolate/serializers.py
+++ amr_tv/isolate/serializers.py
@@ -1,9 +1,9 @@
-from rest_framework import serializers
+from rest_framework.serializers import HyperlinkedModelSerializer
 
 from amr_tv.isolate.models import Isolate
 
 
-class IsolateSerializer(serializers.HyperlinkedModelSerializer):
+class IsolateSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Isolate
         fields = [
@@ -15,17 +15,3 @@ class IsolateSerializer(serializers.HyperlinkedModelSerializer):
             "host",
             "amr_genotypes"
         ]
-
-
-class IsolateLinkSerializer(serializers.Serializer):
-    """wip"""
-    isolate_1 = serializers.CharField()
-    isolate_2 = serializers.CharField()
-    amr_genotypes_1 = serializers.ListField(child=serializers.CharField())
-    amr_genotypes_2 = serializers.ListField(child=serializers.CharField())
-
-    def create(self, validated_data):
-        pass
-
-    def update(self, instance, validated_data):
-        pass
