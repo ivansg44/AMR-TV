700dc6f31936259483411ae578e0e695f0776341
ivansg44 <isgill93@student.ubc.ca>
2020-11-13 10:37:14 -0800

    Override get_queryset in IsolateViewSet (#9)
    * Delete list override

--- amr_tv/isolate/urls.py
+++ amr_tv/isolate/urls.py
@@ -5,7 +5,7 @@ from amr_tv.isolate.views import IsolateViewSet
 
 
 router = routers.DefaultRouter()
-router.register(r"", IsolateViewSet)
+router.register(r"", IsolateViewSet, basename="isolates")
 
 urlpatterns = [
     path("", include(router.urls))
--- amr_tv/isolate/views.py
+++ amr_tv/isolate/views.py
@@ -1,4 +1,3 @@
-from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from amr_tv.isolate.models import Isolate
@@ -6,29 +5,12 @@ from amr_tv.isolate.serializers import IsolateSerializer
 
 
 class IsolateViewSet(ReadOnlyModelViewSet):
-    """wip"""
-    queryset = Isolate.objects.all().order_by("-create_date")
     serializer_class = IsolateSerializer
     lookup_value_regex = '[\w.]+'
 
-    def list(self, request):
-        """Lists all isolates over optionally supplied time period.
-
-        Overrides superclass method. If end_date is supplied,
-        end_date-1 is used.
-
-        :param request: May contain query parameters start_date and
-        end_date.
-        """
-        start_date = request.query_params.get("start_date", "0001-01-01")
-        end_date = request.query_params.get("end_date", "9999-01-01")
-        queryset = self.get_queryset()
-        queryset = queryset.filter(create_date__range=(start_date, end_date))
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        """Optionally return isolates over time period."""
+        queryset = Isolate.objects.all().order_by("-create_date")
+        start_date = self.request.query_params.get("start_date", "0001-01-01")
+        end_date = self.request.query_params.get("end_date", "9999-01-01")
+        return queryset.filter(create_date__range=(start_date, end_date))
