c1fdc6ef5b6b3d5ef2cfc272231312989286c562
Ivan Sohrab Gill <isgill93@student.ubc.ca>
2020-11-13 17:31:32 -0800

    Stub heatmap and new base django image
    * Replaces alpine with buster, because it is easier to install pandas

--- amr_tv/adjacency_matrix/views.py
+++ amr_tv/adjacency_matrix/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-from plotly.graph_objs import Scatter
+from plotly import express as px
 from plotly.offline import plot
 
 
 def adjacency_matrix_view(request):
-    """wip"""
-    x_data = [0,1,2,3]
-    y_data = [x**2 for x in x_data]
-    plot_div = plot([Scatter(x=x_data, y=y_data,
-                             mode='lines', name='test',
-                             opacity=0.8, marker_color='green')],
-                    output_type='div')
-    return render(request, "base.html", {"foo": plot_div})
+    fig = px.imshow([[1, 20, 30],
+                     [20, 1, 60],
+                     [30, 60, 1]])
+    plt_div = plot(fig, output_type='div')
+    return render(request, "base.html", {"foo": plt_div})
