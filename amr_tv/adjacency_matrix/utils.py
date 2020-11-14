91435718cc33f229b3d9d2bed71f071dabc82851
Ivan Sohrab Gill <isgill93@student.ubc.ca>
2020-11-14 14:17:56 -0800

    Revert "wip"
    This reverts commit 921166a1d6127dc08181cc6d1bb8b156b2c632a0.

--- amr_tv/adjacency_matrix/utils.py
+++ amr_tv/adjacency_matrix/utils.py
@@ -1,17 +1,12 @@
-import pandas as pd
-
 from amr_tv.isolate.models import Isolate
 
 
 def foo():
     """wip"""
     # TODO: May not need to use viewsets???
-    isolate_qs = Isolate.objects.all()
-    isolate_qs = \
-        isolate_qs.filter(create_date__range=("2020-10-29", "2020-10-31"))
-    df = pd.DataFrame.from_records(isolate_qs.values())
-
-    df_organism_groups = df.groupby(by="organism_group").groups
-
-    ret = {organism_group: 0 for organism_group in df_organism_groups}
+    queryset = Isolate.objects.all()
+    queryset = queryset.filter(create_date__range=("2020-10-29", "2020-10-31"))
+    organism_groups_list = \
+        list(queryset.values_list('organism_group', flat=True).distinct())
+    ret = {organism_group: 0 for organism_group in organism_groups_list}
     return ret
