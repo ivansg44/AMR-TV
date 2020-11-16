f1d7c88777819da70902c0d768616307d27e3134
Ivan Sohrab Gill <isgill93@student.ubc.ca>
2020-11-15 16:55:41 -0800

    Implemented stub matrix

--- amr_tv/adjacency_matrix/utils.py
+++ amr_tv/adjacency_matrix/utils.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from math import factorial
 
 from django.db.models import Count
@@ -8,35 +9,32 @@ from amr_tv.isolate.models import Isolate, IsolateGenotype
 def foo():
     """wip"""
     # TODO: May not need to use viewsets???
-    date_range = ("2020-10-29", "2020-10-31")
+    date_range = ("2020-10-01", "2020-10-31")
     isolates_qs = Isolate.objects.all().filter(create_date__range=date_range)
     organism_groups_list = \
         isolates_qs.values_list('organism_group', flat=True).distinct()
     ret_val = {organism_group: 0 for organism_group in organism_groups_list}
-    ret = {organism_group: ret_val for organism_group in organism_groups_list}
+    ret = {organism_group: deepcopy(ret_val) for organism_group in organism_groups_list}
 
     isolate_genotypes_qs = \
         IsolateGenotype.objects.all().filter(create_date__range=date_range)
-    total_num_edges = count_edges(isolate_genotypes_qs)
+    shared_genotypes_qs = \
+        isolate_genotypes_qs.values("amr_genotype", "organism_group")
+    shared_genotype_counts_qs = \
+        shared_genotypes_qs.annotate(count=Count("amr_genotype"))
+
+    count_acc = 0
+    amr_genotype_acc = ""
+    organism_groups_acc = set()
+    for e in shared_genotype_counts_qs.order_by("amr_genotype"):
+        if e["amr_genotype"] != amr_genotype_acc:
+            for organism_group in organism_groups_acc:
+                for second_organism_group in organism_groups_acc:
+                    ret[organism_group][second_organism_group] += count_acc
+            count_acc = 0
+            amr_genotype_acc = e["amr_genotype"]
+            organism_groups_acc = set()
+        count_acc += e["count"]
+        organism_groups_acc.add(e["organism_group"])
 
     return ret
-
-
-def count_edges(isolate_genotypes_qs, organism_group=None):
-    """wip"""
-    genotypes_qs = isolate_genotypes_qs.values("amr_genotype")
-    shared_genotype_counts_qs = \
-        genotypes_qs.annotate(total=Count("amr_genotype"))
-    shared_genotype_counts_list = \
-        shared_genotype_counts_qs.values_list("total", flat=True)
-    edge_counts_list = \
-        [count_unique_pairs(count) for count in shared_genotype_counts_list]
-    return sum(edge_counts_list)
-
-
-def count_unique_pairs(num_items):
-    """wip"""
-    if num_items < 2:
-        return 0
-
-    return factorial(num_items) // factorial(2) // factorial(num_items-2)
--- amr_tv/adjacency_matrix/views.py
+++ amr_tv/adjacency_matrix/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
+import numpy as np
 from plotly import express as px
 from plotly.offline import plot
 
 from amr_tv.adjacency_matrix.utils import foo
-from amr_tv.isolate.models import Isolate
 
 
 def adjacency_matrix_view(request):
@@ -11,24 +11,20 @@ def adjacency_matrix_view(request):
     # TODO: May not need to use viewsets???
     foo_ret = foo()
     organism_groups_list = list(foo_ret.keys())
-    fig = px.imshow([[1, 20, 30, 1, 20, 30, 1, 20, 30, 1, 20, 30, 1, 20],
-                     [20, 1, 60, 20, 1, 60, 20, 1, 60, 20, 1, 60, 20, 1],
-                     [30, 60, 1, 30, 60, 1, 30, 60, 1, 30, 60, 1, 30, 60],
-                     [1, 20, 30, 1, 20, 30, 1, 20, 30, 1, 20, 30, 1, 20],
-                     [20, 1, 60, 20, 1, 60, 20, 1, 60, 20, 1, 60, 20, 1],
-                     [30, 60, 1, 30, 60, 1, 30, 60, 1, 30, 60, 1, 30, 60],
-                     [1, 20, 30, 1, 20, 30, 1, 20, 30, 1, 20, 30, 1, 20],
-                     [20, 1, 60, 20, 1, 60, 20, 1, 60, 20, 1, 60, 20, 1],
-                     [30, 60, 1, 30, 60, 1, 30, 60, 1, 30, 60, 1, 30, 60],
-                     [1, 20, 30, 1, 20, 30, 1, 20, 30, 1, 20, 30, 1, 20],
-                     [20, 1, 60, 20, 1, 60, 20, 1, 60, 20, 1, 60, 20, 1],
-                     [30, 60, 1, 30, 60, 1, 30, 60, 1, 30, 60, 1, 30, 60],
-                     [1, 20, 30, 1, 20, 30, 1, 20, 30, 1, 20, 30, 1, 20],
-                     [20, 1, 60, 20, 1, 60, 20, 1, 60, 20, 1, 60, 20, 1],
-                     ],
+    organism_groups_count = len(organism_groups_list)
+    data = np.zeros((organism_groups_count, organism_groups_count))
+    for i in range(organism_groups_count):
+        for j in range(organism_groups_count):
+            first_organism_group = organism_groups_list[i]
+            second_organism_group = organism_groups_list[j]
+            data[i][j] = foo_ret[first_organism_group][second_organism_group]
+    fig = px.imshow(data,
                     x=organism_groups_list,
-                    y=organism_groups_list
+                    y=organism_groups_list,
+                    color_continuous_scale="Greens"
                     )
     fig.update_xaxes(side="top")
-    plt_div = plot(fig, output_type='div')
+    fig.update_layout(width=1000,
+                      height=1000)
+    plt_div = plot(fig, output_type='div', config={"responsive": True})
     return render(request, "base.html", {"foo": plt_div})
