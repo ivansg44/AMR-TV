44698d55dd05e265842f3524749a28951c8d86fd
Ivan Sohrab Gill <isgill93@student.ubc.ca>
2020-11-24 14:49:38 -0800

    Get rid of duplicates and make accurate network

--- amr_tv/node_link_diagram/utils.py
+++ amr_tv/node_link_diagram/utils.py
@@ -21,11 +21,29 @@ def filter_transmission_events(query_params, transmission_events):
 
 def get_transmission_network(transmission_events):
     """TODO: ..."""
+    node_indices_dict = \
+        get_transmission_network_node_indices_dict(transmission_events)
     graph = nx.Graph()
-    count = 1
     for event in transmission_events:
-        graph.add_node(count)
-        graph.add_node(count + 1)
-        graph.add_edge(count, count + 1)
-        count += 2
+        node_index_one = node_indices_dict[str(event[0:3])]
+        node_index_two = node_indices_dict[str(event[3:])]
+        graph.add_node(node_index_one)
+        graph.add_node(node_index_two)
+        graph.add_edge(node_index_one, node_index_two)
     return graph
+
+
+def get_transmission_network_node_indices_dict(transmission_events):
+    """TODO: ..."""
+    node_indices_dict = {}
+    count = 0
+    for event in transmission_events:
+        str_node_one = str(event[0:3])
+        str_node_two = str(event[3:])
+        if str_node_one not in node_indices_dict:
+            node_indices_dict[str_node_one] = count
+            count += 1
+        if str_node_two not in node_indices_dict:
+            node_indices_dict[str_node_two] = count
+            count += 1
+    return node_indices_dict
