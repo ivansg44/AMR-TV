e8452c4635c1badba5ceff1c915f2a9e547f90af
Ivan Sohrab Gill <isgill93@student.ubc.ca>
2020-11-28 14:07:18 -0800

    Add stub table on node select
    * Used example from
      https://plotly.com/python/table/#use-a-pandas-dataframe

--- amr_tv/node_detail_table/views.py
+++ amr_tv/node_detail_table/views.py
@@ -1,6 +1,23 @@
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.offline import plot
+
 from django.shortcuts import HttpResponse
 
 
 def node_detail_table_view(request):
     """TODO: ..."""
-    return HttpResponse("foo")
+    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv')
+
+    fig = go.Figure(data=[go.Table(
+        header=dict(values=list(df.columns),
+                    fill_color='paleturquoise',
+                    align='left'),
+        cells=dict(values=[df.Rank, df.State, df.Postal, df.Population],
+                   fill_color='lavender',
+                   align='left'))
+    ])
+
+    plot_div = plot(fig, output_type="div")
+
+    return HttpResponse(plot_div)
