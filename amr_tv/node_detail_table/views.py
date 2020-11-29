010d193c521842171fb1daabbb16d5ec284da80f
ivansg44 <isgill93@student.ubc.ca>
2020-11-28 18:49:48 -0800

    Clean up (#15)

--- amr_tv/node_detail_table/views.py
+++ amr_tv/node_detail_table/views.py
@@ -15,30 +15,30 @@ def node_detail_table_view(request):
     query_params = dict(request.GET)
     organism_group = query_params["organism_group"][0]
     amr_genotypes = query_params["amr_genotypes[]"]
+
     node_detail_qs = Isolate.objects.filter(
         create_date__range=request.session["date_range"],
         organism_group=organism_group,
         amr_genotypes=amr_genotypes
     )
+    node_detail_values_list = list(node_detail_qs.values_list(*_headers))
+    df = pd.DataFrame(node_detail_values_list, columns=_headers)
 
-    df = pd.DataFrame(list(node_detail_qs.values_list(*_headers)), columns=_headers)
+    header_dict = {"values": _headers, "align": "left"}
+    cells_dict = {
+        "values": [df[header] for header in _headers],
+        "align": "left",
+        "height": 24
+    }
+    table_trace = go.Table(header=header_dict, cells=cells_dict)
 
     fig = go.Figure(
-        data=[go.Table(
-            header=dict(values=_headers,
-                        fill_color='paleturquoise',
-                        align='left'),
-            cells=dict(values=[df[header] for header in _headers],
-                       fill_color='lavender',
-                       align='left',
-                       height=24,
-                       ))],
-        layout={
+        data=[table_trace],
+        layout= {
             "margin": {"l": 0, "r": 0, "t": 0, "b": 0},
             "font": {"size": 14}
         }
     )
-
     plot_div = plot(fig, output_type="div")
 
     return JsonResponse({
