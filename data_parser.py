b4c184bbecde0c4ceb29fcadd6c678004019821e
Ivan S Gill <isgill93@student.ubc.ca>
2021-07-24 13:13:45 -0700

    Dynamically generate x node vals

--- data_parser.py
+++ data_parser.py
@@ -16,9 +16,20 @@ def get_app_data(samples_tsv_path, transmissions_tsv_path):
         for row in reader:
             transmissions_data_dict[row.pop("transmission_id")] = row
     app_data = {
+        "main_fig_nodes_x": get_main_fig_nodes_x(samples_data_dict),
         "main_fig_nodes_y": get_main_fig_nodes_y(samples_data_dict)
     }
     return app_data
+
+
+def get_main_fig_nodes_x(samples_data_dict):
+    """TODO"""
+    sample_dates_list = [v["sample_date"] for v in samples_data_dict.values()]
+    sorted_sample_dates_list = sorted(sample_dates_list)
+    sample_date_x_vals_dict = \
+        {e: i+1 for i, e in enumerate(dict.fromkeys(sorted_sample_dates_list))}
+    main_fig_nodes_x = [sample_date_x_vals_dict[x] for x in sample_dates_list]
+    return main_fig_nodes_x
 
 
 def get_main_fig_nodes_y(samples_data_dict):
