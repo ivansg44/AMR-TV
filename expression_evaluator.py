b9e271f17f6c3a9c2e7aac631d15ee328a14037b
Ivan S Gill <isgill93@student.ubc.ca>
2022-05-25 13:14:54 -0700

    Module for evaluating str expressions

---
+++ expression_evaluator.py
@@ -0,0 +1,34 @@
+"""Module for evaluating str expressions.
+
+Significantly influenced by 
+https://stackoverflow.com/a/9558001/11472358.
+"""
+
+import ast
+import operator as op
+
+# Supported operators
+OPERATORS = {
+    ast.Add: op.add,
+    ast.Sub: op.sub,
+    ast.Mult: op.mul,
+    ast.Div: op.truediv,
+    ast.USub: op.neg
+}
+
+
+def eval_expr(expr):
+    """TODO"""
+    return eval_(ast.parse(expr, mode='eval').body)
+
+
+def eval_(node):
+    """TODO"""
+    if isinstance(node, ast.Num):
+        return node.n
+    elif isinstance(node, ast.BinOp):
+        return OPERATORS[type(node.op)](eval_(node.left), eval_(node.right))
+    elif isinstance(node, ast.UnaryOp):
+        return OPERATORS[type(node.op)](eval_(node.operand))
+    else:
+        raise TypeError(node)
