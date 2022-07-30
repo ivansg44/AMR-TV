3f6f01e03f740204c99d2862697fdc7b4ad4b911
ivansg44 <isgill93@student.ubc.ca>
2022-07-30 10:12:52 -0700

    Remove scaling to link parallel translation (#28)
    * Also remove some unnecessary spacing elsewhere

--- app.py
+++ app.py
@@ -355,7 +355,7 @@ def update_main_viz(selected_nodes, _, sample_file_contents,
         raise RuntimeError(msg)
 
     main_fig_style = {"height": app_data["main_fig_height"],
-                                "width": app_data["main_fig_width"]}
+                      "width": app_data["main_fig_width"]}
     node_symbol_legend_fig = get_node_symbol_legend_fig(app_data)
     link_legend_fig = get_link_legend_fig(app_data)
     node_color_legend_fig = get_node_color_legend_fig(app_data)
