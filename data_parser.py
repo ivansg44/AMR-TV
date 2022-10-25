e545dbee9533c6800047b38c8450c82259f179b7
Ivan S Gill <isgill93@student.ubc.ca>
2022-10-25 12:16:11 -0700

    Process matrix vals in weight exps
    * Also process sample ids again

--- data_parser.py
+++ data_parser.py
@@ -61,12 +61,16 @@ def get_app_data(sample_file_base64_str, config_file_base64_str,
     config_file_str = b64decode(config_file_base64_str).decode("utf-8")
     config_file_dict = loads(config_file_str)
 
-    matrix_file_str = b64decode(matrix_file_base64_str).decode("utf-8")
-    matrix_file_df = pd.read_csv(StringIO(matrix_file_str),
-                                 sep="\t",
-                                 index_col=0)
+    if matrix_file_base64_str:
+        matrix_file_str = b64decode(matrix_file_base64_str).decode("utf-8")
+        matrix_file_df = pd.read_csv(StringIO(matrix_file_str),
+                                     sep="\t",
+                                     index_col=0)
+    else:
+        matrix_file_df = None
 
     sample_data_dict = get_sample_data_dict(sample_file_str,
+                                            config_file_dict["sample_id"],
                                             config_file_dict["delimiter"],
                                             config_file_dict["date_attr"],
                                             config_file_dict["date_input"],
@@ -153,7 +157,8 @@ def get_app_data(sample_file_base64_str, config_file_base64_str,
         links_config=config_file_dict["links_config"],
         primary_y=config_file_dict["y_axes"][0],
         links_across_primary_y=config_file_dict["links_across_primary_y"],
-        max_day_range=config_file_dict["max_day_range"]
+        max_day_range=config_file_dict["max_day_range"],
+        matrix_file_df=matrix_file_df
     )
 
     main_fig_nodes_y_dict = get_main_fig_nodes_y_dict(
@@ -386,9 +391,9 @@ def sorting_key(track):
     return ret
 
 
-def get_sample_data_dict(sample_file_str, delimiter, date, date_input,
-                         date_output, null_vals):
-    """Parse sample data file into dict obj.
+def get_sample_data_dict(sample_file_str, sample_id_attr, delimiter, date,
+                         date_input, date_output, null_vals):
+    """Parse sample data file into dict obj.TODO
 
     :param sample_file_str: Str corresponding to contents of user
         uploaded sample file.
@@ -419,7 +424,7 @@ def get_sample_data_dict(sample_file_str, delimiter, date, date_input,
         row["datetime_obj"] = datetime.strptime(row[date], date_input)
         row[date] = row["datetime_obj"].strftime(date_output)
 
-        sample_data_dict[i] = row
+        sample_data_dict[row[sample_id_attr]] = row
     return sample_data_dict
 
 
@@ -494,7 +499,8 @@ def get_node_color_attr_dict(node_color_attr_list):
 
 
 def get_sample_links_dict(sample_data_dict, links_config, primary_y,
-                          links_across_primary_y, max_day_range):
+                          links_across_primary_y, max_day_range,
+                          matrix_file_df):
     """Get a dict of all links to viz in main graph.
 
     The keys in the dict are different link labels. The values are a
@@ -523,7 +529,7 @@ def get_sample_links_dict(sample_data_dict, links_config, primary_y,
     """
     sample_links_dict = {k: {} for k in links_config}
     sample_list = list(sample_data_dict.keys())
-    regex_obj = compile("!.*?!|@.*?@")
+    regex_obj = compile("!.*?!|@.*?@|{{matrix}}")
 
     def get_sample_attr_list(sample_data, link_config_list, filters):
         # Get the attr values for a sample, corresponding to one of the
@@ -615,6 +621,13 @@ def get_sample_links_dict(sample_data_dict, links_config, primary_y,
                             elif match[0] == "@":
                                 exp_attr = match.strip("@")
                                 return sample_j_data[exp_attr]
+                            elif match == "{{matrix}}":
+                                if matrix_file_df is None:
+                                    msg = "Specified matrix in weight exp, " \
+                                          "but no matrix file provided"
+                                    raise RuntimeError(msg)
+                                matrix_val = matrix_file_df[sample_i][sample_j]
+                                return str(matrix_val)
                             else:
                                 msg = "Unexpected regex match obj when " \
                                       "parsing weight expression: " + match
