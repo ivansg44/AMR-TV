87be14b492b7fe000c1d43f6844460ebd20e684c
Ivan S Gill <ivan_gill@sfu.ca>
2025-08-27 16:47:07 -0700

    kpc3 ecl 719 snapshot done

--- test_app.py
+++ test_app.py
@@ -1,5 +1,17 @@
-from app import update_main_viz
+from base64 import b64encode
+from os import path
+
+from data_parser import get_app_data
+
 
 def test_kpc3_ecl719_snapshot(snapshot):
-    kpc3_ecl719_parsed_data = "foo"
+    with open(path.join("kpc3_data", "kpc3_ecl719_data.csv"), "rb") as fp:
+        data_b64str = b64encode(fp.read())
+    with open(path.join("kpc3_data", "kpc3_ecl719_config.json"), "rb") as fp:
+        config_b64str = b64encode(fp.read())
+    with open(path.join("kpc3_data", "kpc3_ecl719_snp_matrix.csv"), "rb") as fp:
+        matrix_b64str = b64encode(fp.read())
+    kpc3_ecl719_parsed_data = get_app_data(data_b64str,
+                                           config_b64str,
+                                           matrix_b64str)
     assert kpc3_ecl719_parsed_data == snapshot
